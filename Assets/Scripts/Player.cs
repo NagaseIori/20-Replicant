e172105bc96c8e63edbbc7949678fcb6a1c2afeb
Rin <31349569+NagaseIori@users.noreply.github.com>
2024-10-01 21:01:57 +0800

    finish player basic animation
    f__k u animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,47 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float moveSpeed = 5;
+    public float moveSpeed = 5;
 
+
+    private bool isShooting;
     private Rigidbody2D body;
+    private Animator animator;
+    private SpriteRenderer sprRenderer;
     // Start is called before the first frame update
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
 
-        var direction = new Vector2(hAxis, vAxis).normalized;
-        body.velocity = moveSpeed * direction;        
+        var direction = new Vector2(hAxis, vAxis);
+        body.velocity = moveSpeed * direction;
+
+        /// Animation Control
+        
+        if(hAxis != 0 || vAxis != 0)
+        {
+            if (isShooting) animator.Play("PlayerWalk");
+            else animator.Play("PlayerRun");
+        }
+        else
+        {
+            animator.Play("PlayerIdle");
+        }
+
+        /// Sprite Flip Control
+        
+        if(hAxis != 0)
+        {
+            sprRenderer.flipX = hAxis < 0;
+        }
     }
 }
